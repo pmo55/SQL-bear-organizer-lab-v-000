0680196a71e583119ff85d9b2325a4267fb276e4
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-27 03:38:08 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"M", "brown", "tame", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 12,"M", "brown", "tame", 1);
